67238500f9e6fbfac27590f1496ab5827da0e75c
David Alelyunas <dvad123@gmail.com>
2019-12-24 07:38:33 -0500

    add parser form component

---
+++ src/options/AddParserForm.vue
@@ -0,0 +1,63 @@
+<template>
+  <b-card class="addParserCard" title="Add parser">
+    <b-form-group label="Hostname" label-for="hostnameInput">
+      <b-form-input id="hostnameInput" v-model="hostname" />
+    </b-form-group>
+
+    <b-form-group label="Book title parser" label-for="bookTitleInput">
+      <b-form-input id="bookTitleInput" v-model="bookTitleParser" />
+    </b-form-group>
+
+    <b-form-group
+      id="chapterNumberGroup"
+      label="Chapter number parser"
+      label-for="chapterNumberInput"
+    >
+      <b-form-input v-model="chapterNumberParser" />
+    </b-form-group>
+
+    <b-button
+      v-on:click="addParser"
+      v-bind:disabled="!isEnabled(hostname, chapterNumberParser, bookTitleParser)"
+      variant="success"
+    >Add</b-button>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: "AddParserForm",
+  data() {
+    return {
+      hostname: "",
+      bookTitleParser: "",
+      chapterNumberParser: ""
+    };
+  },
+  methods: {
+    addParser() {
+      this.$emit("addParser", {
+        hostname: this.hostname,
+        bookTitleParser: this.bookTitleParser,
+        chapterNumberParser: this.chapterNumberParser
+      });
+      this.hostname = "";
+      this.chapterNumberParser = "";
+      this.bookTitleParser = "";
+    },
+    isEnabled(hostname, chapterNumberParser, bookTitleParser) {
+      return (
+        hostname.length > 0 &&
+        chapterNumberParser.length > 0 &&
+        bookTitleParser.length > 0
+      );
+    }
+  }
+};
+</script>
+
+<style scoped>
+.addParserCard {
+  margin-bottom: 20px;
+}
+</style>
